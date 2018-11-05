a4250a7b82df18bb004d478bdb6c0d8a8b6114ed
jhoni-costa <jhoni_costa@outlook.com>
2018-11-05 19:12:14 -0200

    Slides iniciais

--- app/src/main/java/br/com/jhonicosta/ola_messenger/ApresentationActivity.java
+++ app/src/main/java/br/com/jhonicosta/ola_messenger/ApresentationActivity.java
@@ -1,13 +1,31 @@
 package br.com.jhonicosta.ola_messenger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ApresentationActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class ApresentationActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apresentation);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_01)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_02)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_03)
+                .canGoForward(false)
+                .build());
     }
 }
